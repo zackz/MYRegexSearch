98b734b29f8dcde0d0ea71977513837f96fd8552
Zack Zhou <zackzhh@gmail.com>
2013-03-18 18:18:29 +0800

    v0.2.0

--- MYRegexSearch.php
+++ MYRegexSearch.php
@@ -2,6 +2,12 @@
 /**
  * MYRegexSearch
  * https://github.com/zackz/MYRegexSearch
+ *
+ * History:
+ * 0.2.0
+ * Output elapsed time, and automatically set focus to edit control.
+ * 0.1.0
+ * Basic regular expressions searching.
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
@@ -9,7 +15,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'MYRegexSearch',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'author' => array( 'Zack Zhou' ),
 	'url' => 'https://github.com/zackz/MYRegexSearch',
 	'descriptionmsg'  => 'myregexsearch-desc',
